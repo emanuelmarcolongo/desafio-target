7b2e10c0ca812aaff4a04ccbf708786c2c60afba
EmanuelMarcolongo <emanuelmarcolongo@gmail.com>
2023-03-08 15:41:27 -0300

    feat: question 1

--- desafio.js
+++ desafio.js
@@ -1,1 +1,14 @@
-console.log('Olá mundo');
+function question1 () {
+    let indice = 13;
+    let soma = 0;
+    
+    for (let k = 0; k < indice; k++) {
+        soma = soma + k;
+    }
+
+    console.log('question 1:', soma);
+}
+
+question1();
+
+
