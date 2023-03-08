c62d43170c18e63b67dd71316be0dc2613741cfb
EmanuelMarcolongo <emanuelmarcolongo@gmail.com>
2023-03-08 15:47:41 -0300

    feat: question 2

--- desafio.js
+++ desafio.js
@@ -12,3 +12,26 @@ function question1 () {
 question1();
 
 
+function question2 (numero) {
+    if (numero === 1 || numero === 0) {
+        return console.log(`Questão 2: O número ${numero} pertence a sequencia de Fibonacci`)
+    }
+    let a = 0;
+    let b = 1;
+    let c = a + b;
+
+    while (c <= numero) {
+        if (c === numero) {
+            return console.log(`Questão 2: O número ${numero} pertence a sequencia de Fibonacci`)
+        }
+
+        a = b;
+        b = c;
+        c = a + b;
+    }
+
+    return console.log(`Questão 2: O número ${numero} NÃO pertence a sequencia de Fibonacci`)
+}
+
+question2 (6);
+question2 (34);
